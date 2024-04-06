96cdc7774e59e1080fe30d3d8652da5e0fb6f461
Nikita Ruhtin <4helovekov@gmail.com>
2024-04-06 22:07:09 +0500

    fix: request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import "./styles/App.css";
 import MyButton from "./components/UI/MyButton/MyButton";
 //import MyInput from "./components/UI/MyInput/MyInput";
