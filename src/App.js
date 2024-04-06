e1aaba5923764d723b80464c747773e886a32c57
Nikita Ruhtin <4helovekov@gmail.com>
2024-04-06 22:06:02 +0500

    fix: request

--- src/App.js
+++ src/App.js
@@ -29,104 +29,104 @@ function App() {
             formData.append('image', selectedFiles[i]);
         }
 
-        // fetch('http://localhost:8080/upload', {
-        //     method: 'POST',
-        //     body: formData,
-        //     headers: {
-        //         'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>'
-        //     }
-        //   })
-        //   .then(response => response.blob())
-        //   .then(blob => {
-        //     const imageUrl = URL.createObjectURL(blob);
-        //     console.log(imageUrl)
-        //     const imageElement = document.createElement('img');
-        //     imageElement.src = imageUrl;
-        //     document.body.appendChild(imageElement);
-        //   })
-        //   .catch(error => console.error('Ошибка:', error));
+        fetch('http://localhost:8080/upload', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>'
+            }
+          })
+          .then(response => response.blob())
+          .then(blob => {
+            const imageUrl = URL.createObjectURL(blob);
+            console.log(imageUrl)
+            const imageElement = document.createElement('img');
+            imageElement.src = imageUrl;
+            document.body.appendChild(imageElement);
+          })
+          .catch(error => console.error('Ошибка:', error));
     
-        try {
-            const formData = new FormData();
-            for (let i = 0; i < selectedFiles.length; i++) {
-                formData.append('image', selectedFiles[i]);
-            }
-            console.log(formData)
-            for (const entry of formData.entries()) {
-                console.log(entry[1]);
-            }
+        // try {
+        //     const formData = new FormData();
+        //     for (let i = 0; i < selectedFiles.length; i++) {
+        //         formData.append('image', selectedFiles[i]);
+        //     }
+        //     console.log(formData)
+        //     for (const entry of formData.entries()) {
+        //         console.log(entry[1]);
+        //     }
   
-            const response = await axios.post('http://localhost:8080/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }, responseType: "image/jpeg"
-            });
-            const res = response.blob
-            let imageUrl = URL.createObjectURL(response.blob);
-            console.log(imageUrl)
-            imageUrl = URL.createObjectURL(response.blob);
-            console.log(imageUrl)
-            imageUrl = URL.createObjectURL(res);
-            console.log(imageUrl)
-            // Делайте что-то с imageUrl, например, присвойте его src свойству изображения.
-            // console.log(response)
-            // console.log(response.data)
-            // const blobb = new Blob([response.data], {
-            //     type: "image/jpeg",
-            // });
-            // console.log(blobb)
-            // try {
-            //     const url = URL.createObjectURL(blobb);
-            //     console.log("{image: url}", {image: url})
-            //     console.log("url", url)
-            // } catch {
+        //     const response = await axios.post('http://localhost:8080/upload', formData, {
+        //         headers: {
+        //             'Content-Type': 'multipart/form-data'
+        //         }, responseType: "image/jpeg"
+        //     });
+        //     const res = response.blob
+        //     let imageUrl = URL.createObjectURL(response.blob);
+        //     console.log(imageUrl)
+        //     imageUrl = URL.createObjectURL(response.blob);
+        //     console.log(imageUrl)
+        //     imageUrl = URL.createObjectURL(res);
+        //     console.log(imageUrl)
+        //     // Делайте что-то с imageUrl, например, присвойте его src свойству изображения.
+        //     // console.log(response)
+        //     // console.log(response.data)
+        //     // const blobb = new Blob([response.data], {
+        //     //     type: "image/jpeg",
+        //     // });
+        //     // console.log(blobb)
+        //     // try {
+        //     //     const url = URL.createObjectURL(blobb);
+        //     //     console.log("{image: url}", {image: url})
+        //     //     console.log("url", url)
+        //     // } catch {
 
-            // }
-            // const base64String = response.data;
-            // console.log("base64String", base64String)
-            // const byteCharacters = atob(base64String);
-            // console.log("byteCharacters", byteCharacters)
-            // let byteArray = new Uint8Array(byteCharacters.length);
-            // console.log("byteArray", byteArray)
-            // for (var i = 0; i < byteCharacters.length; i++) {
-            //     byteArray[i] = byteCharacters.charCodeAt(i);
-            // }
-            // const blob = new Blob([byteArray], { type: 'image/png' });
-            // try {
-            //     const imageUrl = URL.createObjectURL(blob);
-            //     const img = document.createElement('img');
-            //     img.src = imageUrl;
-            //     document.body.appendChild(img)
-            // } catch {
+        //     // }
+        //     // const base64String = response.data;
+        //     // console.log("base64String", base64String)
+        //     // const byteCharacters = atob(base64String);
+        //     // console.log("byteCharacters", byteCharacters)
+        //     // let byteArray = new Uint8Array(byteCharacters.length);
+        //     // console.log("byteArray", byteArray)
+        //     // for (var i = 0; i < byteCharacters.length; i++) {
+        //     //     byteArray[i] = byteCharacters.charCodeAt(i);
+        //     // }
+        //     // const blob = new Blob([byteArray], { type: 'image/png' });
+        //     // try {
+        //     //     const imageUrl = URL.createObjectURL(blob);
+        //     //     const img = document.createElement('img');
+        //     //     img.src = imageUrl;
+        //     //     document.body.appendChild(img)
+        //     // } catch {
 
-            // }
+        //     // }
 
-            // const base64String2 = btoa(
-            //     new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), '')
-            // );
-            // //Создаем URL для изображения
-            // const imageUrl2 = URL.createObjectURL(response.data.blob());
-            // const imageUr3 = `data:${response.headers['content-type']};base64,${base64String2}`
-            // console.log("imageUrl2", imageUrl2)
-            // console.log("imageUr3", imageUr3)
-            // // Создаем элемент изображения и отображаем его на странице
-            // // const imageElement = document.createElement('img');
-            // // imageElement.src = imageUrl;
-            // // document.body.appendChild(imageElement);
+        //     // const base64String2 = btoa(
+        //     //     new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), '')
+        //     // );
+        //     // //Создаем URL для изображения
+        //     // const imageUrl2 = URL.createObjectURL(response.data.blob());
+        //     // const imageUr3 = `data:${response.headers['content-type']};base64,${base64String2}`
+        //     // console.log("imageUrl2", imageUrl2)
+        //     // console.log("imageUr3", imageUr3)
+        //     // // Создаем элемент изображения и отображаем его на странице
+        //     // // const imageElement = document.createElement('img');
+        //     // // imageElement.src = imageUrl;
+        //     // // document.body.appendChild(imageElement);
 
-            // console.log(response)
-            // console.log(response.data?.blob())
-            // console.log(response.request)
+        //     // console.log(response)
+        //     // console.log(response.data?.blob())
+        //     // console.log(response.request)
             
 
-            // console.log('Response:', response.data);
-            // const imageUrl = response.data.imageUrl;
-            // const imageElement = document.createElement('img');
-            // imageElement.src = imageUrl;
-            // document.body.appendChild(imageElement);
-        } catch (error) {
-            console.error('Error:', error);
-        }
+        //     // console.log('Response:', response.data);
+        //     // const imageUrl = response.data.imageUrl;
+        //     // const imageElement = document.createElement('img');
+        //     // imageElement.src = imageUrl;
+        //     // document.body.appendChild(imageElement);
+        // } catch (error) {
+        //     console.error('Error:', error);
+        // }
     };
 
     return (
