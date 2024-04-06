d6f3587d7db8a7a465f038354131be7e665dfb3e
Nikita Ruhtin <4helovekov@gmail.com>
2024-04-06 16:12:00 +0500

    fix: request

--- src/App.js
+++ src/App.js
@@ -37,11 +37,37 @@ function App() {
                     'Content-Type': 'multipart/form-data'
                 }
             });
+
+
+            if (!response.ok) {
+                throw new Error('Ошибка при получении изображения');
+            }
+            // Получаем заголовок Content-Type
+            const contentType = response.headers.get('Content-Type');
+            // Если тип содержимого не является изображением JPEG, выбрасываем ошибку
+            if (!contentType || !contentType.includes('image/jpeg')) {
+                throw new Error('Неподдерживаемый тип изображения');
+            }
+          
+            // Преобразуем ответ в Blob (двоичные данные)
             console.log('Response:', response.data);
-            const imageUrl = response.data.imageUrl;
-            const imageElement = document.createElement('img');
-            imageElement.src = imageUrl;
-            document.body.appendChild(imageElement);
+            response.blob();
+            console.log('Response:', response.data);
+            
+            // Создаем URL для изображения
+            const imageUrl2 = URL.createObjectURL(response.blob());
+              
+            // Создаем элемент изображения и отображаем его на странице
+            const imageElement2 = document.createElement('img');
+            imageElement2.src = imageUrl2;
+            document.body.appendChild(imageElement2);
+            
+
+            // console.log('Response:', response.data);
+            // const imageUrl = response.data.imageUrl;
+            // const imageElement = document.createElement('img');
+            // imageElement.src = imageUrl;
+            // document.body.appendChild(imageElement);
         } catch (error) {
             console.error('Error:', error);
         }
